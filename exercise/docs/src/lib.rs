ee8df16ee903caf7b3ff2f62df883ddb7bc3359d
Keeeweee <epsilonmajorquezero@gmail.com>
2023-04-09 04:56:12 +0200

    Solve Docs exercise

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -15,8 +15,27 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/*!
+A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus
+Cucurbita that has culinary and cultural significance but no agreed upon botanical or scientific
+meaning. The term pumpkin is sometimes used interchangeably with "squash" or "winter squash",
+and is commonly used for cultivars of Cucurbita argyrosperma, Cucurbita ficifolia, Cucurbita maxima,
+Cucurbita moschata, and Cucurbita pepo.
+
+![Pumpkin image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+**/
+
+/**
+Big orange thing
+
+# Recipies
+
+Recipies will be coming soonish
+**/
 pub struct Pumpkin {
+    /// `roundess` is a percentage
     pub roundness: f32,
+    /// `orangeness` must be a number between 8 and 27
     pub orangeness: i32,
 }
 
@@ -24,16 +43,19 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// If you smash the pumpkin, it will be gone. Then it can't be used for pie. :'-(
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for the "orangeness" field in the [Pumpkin] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
 // gets generated as well.  Hint: `cargo doc -h` will show you all the relevant options.
+// -> --document-private-items  Document private items
 
 /// For internal use only. In fact, this documentation is so private that it won't be generated.
 /// At least not by default. But if you pass the correct option in, it will magically appear!
